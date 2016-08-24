8185c5e3a30953fe1053ccb193bdb74c2633b983
ShenWu <sunday781019@gmail.com>
2016-08-24 22:17:25 +0800

    commit1

---
+++ Assets/Script/Test01.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test01 : MonoBehaviour {
+GameObject obj1;
+	// Use this for initialization
+	void Start () {
+		CallTest02();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void CallTest02(){
+		//GameObject t2 = GameObject.Find("Test02");
+		Test02 t2 = GameObject.Find("Test02").GetComponent<Test02>();
+		GameObject aa;
+		
+		Debug.Log(t2.gameObject.name);
+	}
+}
+
---
+++ Assets/Script/Test02.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void ThisIsTest02(){
+			Debug.Log("I'm Test02");
+
+	}
+}
